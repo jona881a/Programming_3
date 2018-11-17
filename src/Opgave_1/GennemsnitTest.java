1c77354b45d1d804458918c9c1efa8f9d42b5d15
Erik Vangslev <erikvangslev4000@gmail.com>
2018-11-17 15:01:28 +0100

    Added a test

--- src/Opgave_1/GennemsnitTest.java
+++ src/Opgave_1/GennemsnitTest.java
@@ -2,7 +2,7 @@ package Opgave_1;
 
 public class GennemsnitTest {
 	public static void main(String[] args) {
-		int[] a = {0,1};
+		int[] a = {0,-1, 69, 420, 1337};
 		double g = Gennemsnit.beregn(a);
 		System.out.println(g);
 	}
