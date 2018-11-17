882d93bef5659d3f0b77ca88f522434fc50c6655
Erik Vangslev <erikvangslev4000@gmail.com>
2018-11-17 15:00:35 +0100

    Added the last comments and optimized code

--- src/Opgave_1/Gennemsnit.java
+++ src/Opgave_1/Gennemsnit.java
@@ -3,25 +3,30 @@ package Opgave_1;
 public class Gennemsnit {
 
 	public static double beregn(int[] tal) {
-		
-		// Before anything, we check to see if the int array is valid
-		if (tal.length == 0) {throw new IllegalArgumentException("The array can't be empty!");}
-		
-		// The variables are declared
-		// i is created here so that it can be used later outside of the for loop
-		// sum is going to be all the numbers in the array added up
-		double sum = 0;
-		int i;
-		
-		// The sum of the array is calculated with a simple for loop
-		for (i = 0; i < tal.length; i++) {
-			sum = sum + tal[i];
+
+		// Before anything, we check to see if the int array is valid.
+		// There will be a TON of problems if the array is empty, so in that case an
+		// exception is thrown.
+		if (tal.length == 0) {
+			throw new IllegalArgumentException("The array can't be empty!");
 		}
 
-		// i is going to be equal to the amount of elements in the array
-		// Dividing the sum with the amount of elements in the array gives us the average
+		// The variables are declared.
+		// i is created here so that it can be used later outside of the for loop.
+		// sum is going to be all the numbers in the array added up.
+		double sum = 0;
+		int i;
+
+		// The sum of the array is calculated with a simple for loop.
+		for (i = 0; i < tal.length; i++) {
+			sum += tal[i];
+		}
+
+		// i is going to be equal to the amount of elements in the array.
+		// Dividing the sum with the amount of elements in the array
+		// gives us the average.
 		return sum / i;
-		
+
 	}
-	
+
 }
