ce18b9fd89c41272634a25c1fa4880c8b2475f44
Erik Vangslev <erikvangslev4000@gmail.com>
2018-11-17 14:33:14 +0100

    Added some comments

--- src/Opgave_1/Gennemsnit.java
+++ src/Opgave_1/Gennemsnit.java
@@ -8,15 +8,18 @@ public class Gennemsnit {
 		if (tal.length == 0) {throw new IllegalArgumentException("The array can't be empty!");}
 		
 		// The variables are declared
+		// i is created here so that it can be used later outside of the for loop
+		// sum is going to be all the numbers in the array added up
 		double sum = 0;
 		int i;
 		
-		// The sum of the array is calculated
+		// The sum of the array is calculated with a simple for loop
 		for (i = 0; i < tal.length; i++) {
 			sum = sum + tal[i];
 		}
 
 		// i is going to be equal to the amount of elements in the array
+		// By dividing the sum with the amount of elements in the array gives us the average
 		return sum / i;
 		
 	}
